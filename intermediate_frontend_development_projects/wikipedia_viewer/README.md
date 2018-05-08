7c1fa06dc7f628a45a69681ba13b8c22640b887d
花.月夜 <549521498@qq.com>
2018-05-08 21:59:02 +0800

    complete of function 1

--- intermediate_frontend_development_projects/wikipedia_viewer/README.md
+++ intermediate_frontend_development_projects/wikipedia_viewer/README.md
@@ -4,6 +4,8 @@
 ----------
 Q：动画延伸放大镜宽度时，整个放大镜变成诡异的椭圆？
 A：不适用border-radius：50%直接设置圆形。而单独设置四个角的圆弧度
+
+Topic：使用window.location.href或者window.location.assign()连接第三方地址。若需要新开浏览器标签，可使用window.open()
 > A Vue.js project
 
 ## Build Setup
--- intermediate_frontend_development_projects/wikipedia_viewer/src/components/HelloWorld.vue
+++ intermediate_frontend_development_projects/wikipedia_viewer/src/components/HelloWorld.vue
@@ -1,24 +1,33 @@
 <template>
   <div class="hello">
-      <a class="linkTopic link">click hear for random article</a>
-      <div class="searchArea" :class="{active: isActive}" @click="search" >
-        <input class="glass" type="text" />
+      <a class="linkTopic link" @click="searchRandom">click hear for random article</a>
+      <div class="searchArea" :class="{active: isActive}" @click="searchState" >
+        <input class="glass" type="text" v-model="searchVal" @keyup.enter="searchWiki" />
       </div>
       <p class="linkTopic">Click icon to search</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
-      isActive: false
+      isActive: false,
+      searchVal: ''
     }
   },
   methods: {
-    search: function(){
+    searchState: function(){
       this.$set(this, 'isActive', true)
+    },
+    searchRandom: function(){
+      window.open('http://en.wikipedia.org/wiki/Special:Random')
+    },
+    searchWiki: function(){
+      axios.get('https://en.wikipedia.org/w/api.php')
     }
   }
 }
@@ -37,7 +46,7 @@ export default {
   cursor: pointer;
 }
 .searchArea {
-  width: 36px;
+  width: 32px;
   margin: 25px auto;
   border: 4px solid  #D96F32;
   border-top-left-radius: 25px;
@@ -63,6 +72,9 @@ export default {
   width: 100%;
   height: 32px;
   outline: none;
+  border: 0;
+  padding: 0 8px;
+  font-size: 15px;
   background: transparent
 }
 .searchArea.active {
@@ -72,6 +84,10 @@ export default {
 .searchArea.active::before {
   animation: vanish 1s;
   animation-fill-mode: forwards;
+}
+.searchArea.active .glass {
+  visibility: visible;
+  color: #fff;
 }
 @keyframes changeGlass {
   from {width: 32px}
