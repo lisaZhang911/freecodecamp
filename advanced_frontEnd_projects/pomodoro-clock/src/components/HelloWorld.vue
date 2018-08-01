2ef265e1523df055239f1412bb2fa3bab74a4fff
花.月夜 <549521498@qq.com>
2018-08-01 08:32:47 +0800

    计算剩余时间

--- advanced_frontEnd_projects/pomodoro-clock/src/components/HelloWorld.vue
+++ advanced_frontEnd_projects/pomodoro-clock/src/components/HelloWorld.vue
@@ -5,13 +5,13 @@
       <input type="text" v-model="workTime">
       <button @click="increas(workTime)">+</button>
     </div>
-    <div class="circle">
+    <div class="circle" @click="startTime">
       <div class="solidCircle">
-
+        <h1>session</h1>
+        <span v-if="start">hms</span>
+        <span v-else>{{hour}}:{{minute}}:{{second}}</span>
       </div>
-        <div class="inner">
-          {{workTime}}
-        </div>
+      <div class="inner"></div>
     </div>
 
   </div>
@@ -20,10 +20,35 @@
 <script>
 export default {
   name: 'HelloWorld',
+  computed:{
+    hour(){
+      var leftSec = this.workTime * 60 - 1;
+      this.$set(this,'leftSecond',leftSec);
+      var leftHour = Math.floor(leftSec/3600);
+      this.$set(this,'leftHour',leftHour);
+      return leftHour;
+    },
+    minute(){
+      var leftMin = Math.floor((this.leftSecond - this.leftHour*3600)/60);
+      var leftSec = this.leftSecond - leftMin*60;
+      this.$set(this,'leftSecond',leftSec);
+      this.$set(this,'leftMin',leftMin);
+      return leftMin
+    },
+    second(){
+      if(this.leftSecond < 60){
+        return this.leftSecond
+      }
+    }
+  },
   data(){
     return {
+      start: false,
       workTime: 32,
-      breakTime: 10
+      breakTime: 10,
+      leftHour: 0,
+      leftMin: 0,
+      leftSecond: 0
     }
   },
   methods: {
@@ -34,6 +59,9 @@ export default {
     increas(obj){
       var newVal = obj+1;
       this.$set(this,'workTime',newVal)
+    },
+    startTime(){
+      this.$set(this,'start',true)
     }
   }
 }
@@ -46,11 +74,15 @@ export default {
   width:300px;height: 300px;
   margin: 0 auto;
   position: relative;
-  /* border-radius: 50%; */
+  z-index: 1;
+  overflow: hidden;
+  border-radius: 50%;
+  cursor: pointer;
 }
 .circle .solidCircle {
-  width: 250px;height: 250px;
-  border: 2px solid red;
+  box-sizing: border-box;
+  width: 300px;height: 300px;
+  border: 3px solid #333;
   border-radius: 50%;
   position: absolute;
   top: 0;bottom: 0;left: 0;right: 0;
@@ -59,7 +91,7 @@ export default {
 }
 .circle .inner {
   box-sizing: border-box;
-  height: 10px;width: 100%;
+  height: 100px;width: 100%;
   background: #99CC00;
   position: absolute;
   bottom: 0
